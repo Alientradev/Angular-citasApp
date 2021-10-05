e0445e726e22811dc2780504fd2148c957b7d415
Julian <alientradev@gmail.com>
2021-10-05 11:28:41 -0500

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+//Componentes
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ListCitasComponent } from './components/list-citas/list-citas.component';
+import { CrearCitaComponent } from './components/crear-cita/crear-cita.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ListCitasComponent,
+    CrearCitaComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
